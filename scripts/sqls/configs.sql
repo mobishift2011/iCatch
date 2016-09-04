b444decbd59c7f0a7ca7861bb5c72c8887f62e5c
huang <ethan.huang@gllue.com>
2016-09-04 15:03:00 +0800

    update

--- scripts/sqls/configs.sql
+++ scripts/sqls/configs.sql
@@ -1,6 +1,9 @@
 insert into `configvalue` (`title`, `type`, `value`) values
 ('timezone', 'timezone', 'UTC'),
-('profile', 'profile', 'Default'),
+('profile', 'profile', '默认'),
 ('fileHash', 'fileHash', 'md5'),
 ('quarantineMessage', 'quarantine', ''),
 ('quarantineWhitelist', 'quarantine', '');
+
+insert into `profile` (`title`, `description`, `addedBy_id`, `dateAdded`, `deleted`)
+values ('默认', '系统默认', (select id from user limit 1), now(), 0);
